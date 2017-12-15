fba602d25883e0d5314f47f53ff612164bb2c501
Gopal Sharma <gopalindians@gmail.com>
2017-12-15 15:16:25 +0530

    Added Total Files and File Size

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-ini_set( 'display_errors', '1' );
-error_reporting( E_ALL ); ?>
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+$directory = scandir('.', SCANDIR_SORT_NONE);
+$totalSize = 0;
+
+foreach ($directory as $item) {
+    $totalSize += filesize($item);
+}
+
+$totalSize = formatSizeUnits($totalSize);
+
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -14,7 +26,13 @@ error_reporting( E_ALL ); ?>
           integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
+
 <h1 class="text-center">Download List!</h1>
+<div class="container">
+    <h2>Total files:<span class="badge badge-secondary"><?= count($directory) ?></span></h2>
+    <h2>Total size:<span class="badge badge-secondary"><?= $totalSize ?></span></h2>
+
+</div>
 
 <div class="container">
     <table class="table table-dark">
@@ -28,93 +46,95 @@ error_reporting( E_ALL ); ?>
         </tr>
         </thead>
         <tbody>
-		<?php foreach ( scandir( '.', SCANDIR_SORT_NONE ) as $key => $dir ): ?>
-			<?php if ( $dir === '.' || $dir === '..' ): ?>
-			<?php else: ?>
+        <?php foreach ($directory as $key => $dir): ?>
+            <?php if ($dir === '.' || $dir === '..'): ?>
+            <?php else: ?>
                 <tr>
                     <th scope="row"><?= $key; ?></th>
                     <td><a href="/<?= $dir; ?>" download><?= $dir; ?> <img download style="cursor:pointer;" width="20"
                                                                            src="//upload.wikimedia.org/wikipedia/commons/1/1e/Download-Icon.png"></a>
                     </td>
-                    <td><?= formatSizeUnits( filesize( $dir ) ); ?></td>
+                    <td><?= formatSizeUnits(filesize($dir)); ?></td>
 
 
-					<?php if ( get_file_extension( $dir ) === 'exe' ): ?>
+                    <?php if (get_file_extension($dir) === 'exe'): ?>
                         <td><img width="20"
-                                 src="//maxcdn.icons8.com/office/PNG/512/Files/exe-512.png"><?= get_file_extension( $dir ); ?>
+                                 src="//maxcdn.icons8.com/office/PNG/512/Files/exe-512.png"><?= get_file_extension($dir); ?>
                         </td>
-					<?php elseif ( get_file_extension( $dir ) === 'png' ): ?>
+                    <?php elseif (get_file_extension($dir) === 'png'): ?>
                         <td><img width="20"
-                                 src="//www.iconhot.com/icon/png/file-icons-vs-2/256/png-36.png"><?= get_file_extension( $dir ); ?>
+                                 src="//www.iconhot.com/icon/png/file-icons-vs-2/256/png-36.png"><?= get_file_extension($dir); ?>
                         </td>
 
-					<?php elseif ( get_file_extension( $dir ) === 'php' ): ?>
+                    <?php elseif (get_file_extension($dir) === 'php'): ?>
                         <td><img width="20"
-                                 src="//anques.com/wp-content/uploads/2017/03/icon-php1-1.png"><?= get_file_extension( $dir ); ?>
+                                 src="//anques.com/wp-content/uploads/2017/03/icon-php1-1.png"><?= get_file_extension($dir); ?>
                         </td>
 
-					<?php elseif ( get_file_extension( $dir ) === 'msi' ): ?>
-                        <td><img width="20" src="//i.stack.imgur.com/ZE6c2.png"><?= get_file_extension( $dir ); ?>
+                    <?php elseif (get_file_extension($dir) === 'msi'): ?>
+                        <td><img width="20" src="//i.stack.imgur.com/ZE6c2.png"><?= get_file_extension($dir); ?>
                         </td>
 
-					<?php elseif ( get_file_extension( $dir ) === 'zip' ): ?>
+                    <?php elseif (get_file_extension($dir) === 'zip'): ?>
                         <td><img width="20"
-                                 src="//www.freeiconspng.com/uploads/file-zip-icon-png-17.png"><?= get_file_extension( $dir ); ?>
+                                 src="//www.freeiconspng.com/uploads/file-zip-icon-png-17.png"><?= get_file_extension($dir); ?>
                         </td>
 
-					<?php elseif ( get_file_extension( $dir ) === 'jpg' ): ?>
+                    <?php elseif (get_file_extension($dir) === 'jpg'): ?>
                         <td><img width="20"
-                                 src="//icons.iconarchive.com/icons/zerode/plump/256/Filetype-jpg-icon.png"><?= get_file_extension( $dir ); ?>
+                                 src="//icons.iconarchive.com/icons/zerode/plump/256/Filetype-jpg-icon.png"><?= get_file_extension($dir); ?>
                         </td>
-					<?php elseif ( get_file_extension( $dir ) === 'ini' ): ?>
+                    <?php elseif (get_file_extension($dir) === 'ini'): ?>
                         <td><img width="20"
-                                 src="//downloadicons.net/sites/default/files/ini-file-icon-96066.png"><?= get_file_extension( $dir ); ?>
+                                 src="//downloadicons.net/sites/default/files/ini-file-icon-96066.png"><?= get_file_extension($dir); ?>
                         </td>
-					<?php else: ?>
-                        <td><img width="20" src=""><?= get_file_extension( $dir ); ?></td>
-					<?php endif; ?>
-                    <td><?= gmdate( 'M d, D Y, h:m	:s a', filemtime( $dir ) ); ?></td>
+                    <?php else: ?>
+                        <td><img width="20" src=""><?= get_file_extension($dir); ?></td>
+                    <?php endif; ?>
+                    <td><?= gmdate('M d, D Y, h:m	:s a', filemtime($dir)); ?></td>
                 </tr>
-			<?php endif; ?>
-		<?php endforeach; ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
         </tbody>
     </table>
 
-	<?php
-	function formatSizeUnits( $bytes ) {
-		if ( $bytes >= 1073741824 ) {
-			$bytes = number_format( $bytes / 1073741824, 2 ) . ' GB';
-		} elseif ( $bytes >= 1048576 ) {
-			$bytes = number_format( $bytes / 1048576, 2 ) . ' MB';
-		} elseif ( $bytes >= 1024 ) {
-			$bytes = number_format( $bytes / 1024, 2 ) . ' KB';
-		} elseif ( $bytes > 1 ) {
-			$bytes .= ' bytes';
-		} elseif ( $bytes === 1 ) {
-			$bytes .= ' byte';
-		} else {
-			$bytes = '0 bytes';
-		}
-		return $bytes;
-	}
+    <?php
+    function formatSizeUnits($bytes)
+    {
+        if ($bytes >= 1073741824) {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        } elseif ($bytes >= 1048576) {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        } elseif ($bytes >= 1024) {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        } elseif ($bytes > 1) {
+            $bytes .= ' bytes';
+        } elseif ($bytes === 1) {
+            $bytes .= ' byte';
+        } else {
+            $bytes = '0 bytes';
+        }
+        return $bytes;
+    }
 
 
-	function get_file_extension( $fileName ) {
-		/*
-		 * "." for extension should be available and not be the first character
-		 * so position should not be false or 0.
-		 */
-		$lastDotPos = strrpos( $fileName, '.' );
-		if ( ! $lastDotPos ) {
-			return false;
-		}
-		return substr( $fileName, $lastDotPos + 1 );
-	}
+    function get_file_extension($fileName)
+    {
+        /*
+         * "." for extension should be available and not be the first character
+         * so position should not be false or 0.
+         */
+        $lastDotPos = strrpos($fileName, '.');
+        if (!$lastDotPos) {
+            return false;
+        }
+        return substr($fileName, $lastDotPos + 1);
+    }
 
-	?>
+    ?>
     <footer class="text-center">
-       Nerd mode: On<input type="radio" value="true" name="nerd">
-       Off<input type="radio" checked value="false" name="nerd"> |
+        Nerd mode: On<input type="radio" value="true" name="nerd">
+        Off<input type="radio" checked value="false" name="nerd"> |
         <small>Made by <a target="_blank" href="//github.com/gopalindians">@gopalindians</a></small>
     </footer>
 </div>
